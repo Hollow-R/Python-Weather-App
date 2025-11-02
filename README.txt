531a8f1f6b634d3525b09968dd6d103cf9382cf7
Eren E <88184672+Hollow-R@users.noreply.github.com>
2025-11-02 18:37:19 +0300

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,7 @@
+# Python Weather Application
+
+This project shows the weather in terminal by using OpenWeatherMap API.
+
+You can get your own API key from this link:
+https://openweathermap.org/api
+
---
+++ weather_app.py
@@ -0,0 +1,43 @@
+import requests
+
+API_KEY = "d02bf485997de7f9e70c80262725e8a4"     #Your API key goes here
+BASE_URL = "https://api.openweathermap.org/data/2.5/weather"
+
+def get_weather(city_name):
+    params = {
+        "q": city_name,
+        "appid": API_KEY,
+        "units": "metric",
+        "lang": "eng"
+    }
+    
+    response = requests.get(BASE_URL, params=params)
+    
+    if response.status_code == 200:
+        data = response.json()
+        city = data["name"]
+        temp = data["main"]["temp"]
+        feels_like = data["main"]["feels_like"]
+        humidity = data["main"]["humidity"]
+        desc = data["weather"][0]["description"]
+
+        print(f"\nWeather for {city} :")
+        print(f"Temperature: {temp}°C")
+        print(f"Felt Temperature: {feels_like}°C")
+        print(f"Humidity: {humidity}%")
+        print(f"Status: {desc.capitalize()}\n")
+    else:
+        print("Error\n")
+
+def main():
+    print("=== Weather Application ===")
+    while True:
+        city = input("City name (press q to exit): ")
+        if city.lower() == "q":
+            print("bye")
+            break
+        get_weather(city)
+
+if __name__ == "__main__":
+    main()
+        
